a5dab3ee2bb0b0f1205e0e665a9e70bf2169cdb8
fahsaaulanh <fahsaaula8@gmail.com>
2022-11-19 11:43:03 +0700

    create home scene, and scene manager

---
+++ Assets/Scripts/Scene/Home/HomeScene.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class HomeScene : MonoBehaviour
+{
+    [SerializeField] private Button _playButton, _creditButton, _optionButton, _exitButton;
+    [SerializeField] private GameObject _creditPopup, _optionPopuop, _exitPopup;
+    [SerializeField] private string _nextScene;
+
+    private void Start()
+    {
+        _playButton.onClick.AddListener(StartGame);
+        _optionButton.onClick.AddListener(Option);
+        _exitButton.onClick.AddListener(Exit);
+        _creditButton.onClick.AddListener(Credit);
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene(_nextScene);
+    }
+
+    private void Credit()
+    {
+        _creditPopup.gameObject.SetActive(true);
+    }
+
+    private void Option()
+    {
+        _optionPopuop.gameObject.SetActive(true);
+    }
+
+    private void Exit()
+    {
+        _exitPopup.gameObject.SetActive(true);
+    }
+}
